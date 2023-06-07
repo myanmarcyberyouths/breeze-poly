56b90bcc1a3abbe2c9dc837fa94278e46f218195
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-07 22:46:16 +0630

    refactor: role and permission seeder

--- README.md
+++ README.md
@@ -10,6 +10,5 @@ composer require laravel/passport --with-all-dependencies
 
 ```bash
 php artisan migrate
-php artisan db:seed --class=RoleSeeder # Seeding Roles
-php artisan db:seed --class=PermissionSeeder # Seeding Permissions
+php artisan migrate:fresh --seed  --seeder=RolesAndPermissionsSeeder
 ```
--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -5,16 +5,28 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
-class PermissionSeeder extends Seeder
+class RolesAndPermissionsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
         Permission::create(['name' => 'create events']);
         Permission::create(['name' => 'edit events']);
         Permission::create(['name' => 'delete events']);
+
+        $user = Role::create(['name' => 'user']);
+        $organizer = Role::create(['name' => 'organizer']);
+
+        $user->givePermissionTo(Permission::all());
+        $organizer->givePermissionTo(Permission::all());
+
+
     }
 }
