282a82ed99f3f815c4ce5b219c7953866e701f4b
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-07 22:26:06 +0630

    feat: add permissions seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name' => 'create events']);
+        Permission::create(['name' => 'edit events']);
+        Permission::create(['name' => 'delete events']);
+    }
+}
