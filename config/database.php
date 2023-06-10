2429d9b9b74790346d78890d7ea8c6c5077f4cc6
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-11 05:10:45 +0630

    feat: add endpoint to the password

--- config/database.php
+++ config/database.php
@@ -76,10 +76,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
-            'options' => [
-                'sslmode' => 'require',
-                'endpoint' => env('DB_ENDPOINT', 'your-endpoint-id'),
-            ],
         ],
 
         'sqlsrv' => [
@@ -129,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
