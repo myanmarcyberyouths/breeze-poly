864d1e0c065fd0fd117db4a31337ce69280c4cf3
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-29 04:30:13 +0630

    Revert "feat: add posts feature (#16)"
    This reverts commit 0aa001cfd431f22f39d83f447a61cdecd5821a1c.

--- app/helpers.php
+++ app/helpers.php
@@ -1,27 +1,16 @@
 <?php
 
-use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Validation\ValidationException;
 
 if (!function_exists('json_response')) {
     function json_response($status, $message, $data = null): JsonResponse
     {
-        try{
-            return response()->json([
-                'meta' => [
-                    'status' => $status,
-                    'msg' => $message
-                ],
-                'data' => $data
-            ]);
-        } catch (ValidationException $e) {
-
-            $errors = $e->validator->getMessageBag()->toArray();
-            return response()->json(['errors' => $errors], $e->status);
-
-        } catch (Exception $e) {
-            return response()->json(['message' => 'An error occurred'], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        return response()->json([
+            'meta' => [
+                'status' => $status,
+                'msg' => $message
+            ],
+            'data' => $data
+        ]);
     }
 }
