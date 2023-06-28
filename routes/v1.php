0aa001cfd431f22f39d83f447a61cdecd5821a1c
Wint Phyusin Maung <62115794+Shonn-Shonn@users.noreply.github.com>
2023-06-28 23:28:41 +0630

    feat: add posts feature (#16)

--- routes/v1.php
+++ routes/v1.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\V1\Auth\AuthController;
 use App\Http\Controllers\Api\V1\Auth\EmailValidationController;
 use App\Http\Controllers\Api\V1\Auth\InterestController;
 use App\Http\Controllers\Api\V1\EventController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,11 +18,19 @@ Route::prefix('users')->group(function () {
 });
 
 Route::middleware('auth:api')->group(function () {
+
     Route::group(['prefix' => 'users'], function () {
         Route::get('/me', [AuthController::class, 'getAuthUser']);
-
         Route::post('/sign-out', [AuthController::class, 'logout']);
     });
+
+    Route::group(['prefix' => 'posts'], function() {
+        Route::post('/save',[PostController::class,'save']);
+        Route::post('/favorite',[PostController::class,'favorite']);
+        Route::post('/unfavorite',[PostController::class,'unfavorite']);
+    });
+    
 });
 
 Route::apiResource('/events', EventController::class);
+
