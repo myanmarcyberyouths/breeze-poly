c9b0fcbcd72bcd4f42c833a220a1d487c4ca819d
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-19 19:37:57 +0630

    feat: add event user

--- database/migrations/2023_07_19_123328_create_event_user.php
+++ database/migrations/2023_07_19_123328_create_event_user.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('event_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('event_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('event_id');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
