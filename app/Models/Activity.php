37af38aa3b550788da01b3cd0ae98f3a1cce3ea2
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-08-27 21:36:16 +0630

    feat: add activity model

---
+++ app/Models/Activity.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'action_id',
+        'post_id',
+        'user_id',
+    ];
+}
