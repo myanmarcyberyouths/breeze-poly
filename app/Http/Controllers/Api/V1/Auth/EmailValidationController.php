45d9804668cbf0a3342e6f6cbb22426d67d207e4
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2024-01-16 19:51:16 +0630

    replace http status codes with class constants

--- app/Http/Controllers/Api/V1/Auth/EmailValidationController.php
+++ app/Http/Controllers/Api/V1/Auth/EmailValidationController.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers\Api\V1\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Auth\EmailRequest;
 use App\Http\Requests\V1\Auth\ProfileImageRequest;
+use Illuminate\Http\Response;
 
 class EmailValidationController extends Controller
 {
     public function validateEmail(EmailRequest $request)
     {
-        return json_response(200, "Email is valid");
+        return json_response(Response::HTTP_OK, "Email is valid");
     }
 
     public function validateProfileImage(ProfileImageRequest $request)
     {
-        return json_response(200, "Profile image is valid");
+        return json_response(Response::HTTP_OK, "Profile image is valid");
     }
 }
